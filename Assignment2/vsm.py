038b3ebb1846d663b3a5f8734f507a800f6a2e11
HUANG Jiahao <huangjh43@foxmail.com>
2018-10-06 19:04:13 +0800

    second edition

--- Assignment2/vsm.py
+++ Assignment2/vsm.py
@@ -1,267 +1,327 @@
 #!/usr/bin/python
 
 import math
-import numpy as np
+import time
 
-class Vector:
+# INPUT_PATH = '../data/'
+INPUT_PATH = './'
+OUTPUT_PATH = '../output/'
+
+class Vector(object):
     '''
-        Data structure for document and query
-        when did = -1, the vector object is a query
+        Abstract data structure for document and query, initialize by a list
+        containing the preprocessed passage words: ['Showers', 'continued',
+        'throughout' ...].
+
+        Attrs:
+            did: int, if did = -1, this vector is representing a query.
+            term_frequency: dictionary, map keywords to its frequency.
+            term_index: dictionary, map keywords to a list of positions in
+            the document.
+            weights: dictionary, map keywords to its weights by the scheme
+            of tf / max_tf * idf.
     '''
-    def __init__(self, words, did = -1):
-        self.text = words
+    def __init__(self, text, did = -1):
         self.did = did
         self.term_frequency = {}
-        for word in self.text:
+        self.term_index = {}
+        self.weights = {}
+        for i in range(len(text)):
+            word = text[i]
             if word in self.term_frequency.keys():
                 self.term_frequency[word] += 1
+                self.term_index[word].append(i)
             else:
                 self.term_frequency[word] = 1
+                self.term_index[word] = [i]
+                self.weights[word] = 0.0
 
-        self.max_tf = sorted(self.term_frequency.items(), key = lambda x: x[1])[-1][1]
+    def set_weight(self, word, value):
+        self.weights[word] = value
 
     def get_id(self):
         return self.did
 
-    def get_single_words(self):
-        return self.term_frequency.keys()
+    def get_max_tf(self):
+        return sorted(self.term_frequency.items(), key = lambda x: x[1])[-1][1]
 
     def get_tf(self, term):
-        return self.term_frequency[term]
-
-    def get_max_tf(self):
-        return self.max_tf
-
-class InvertedFile:
-    '''
-        Inverted file index for the preprocessed documents
-    '''
-    def __init__(self, word_file_map, word_indexes):
-        self.index = word_file_map
-        self.posting_list = word_indexes
-
-    def get_documents(self, term):
-        return self.index[term]
-
-    def display(self, term):
-        candidate_passages = self.index[term]
-        out = term + ' ' * (10 - len(term))
-        out += '-> | '
-        for did in candidate_passages:
-            out += 'D' + str(did) + ':'
-            out += str(self.posting_list[did][term][0])
-            for i in range(1, len(self.posting_list[did][term])):
-                out += ',' + str(self.posting_list[did][term][i])
-            out += ' | '
-        print(out)
-
-class VectorSpace:
-    '''
-        Containing the matrix representation for the whole vector space
-    '''
-    def __init__(self, documents, keywords):
-        self.vectorspace = np.zeros((len(documents), len(keywords)))
-        self.documents = documents
-        self.dictionary = {}
-        for i, word in enumerate(keywords):
-            self.dictionary[word] = i
-
-    def compute_df(self, inverted_file):
-        n, m = self.vectorspace.shape
-        self.df = {}
-        self.idf = {}
-        for word in self.dictionary.keys():
-            curr_df = len(inverted_file.get_documents(word))
-            curr_idf = math.log(n / curr_df, 2)
-            self.df[word] = curr_df
-            self.idf[word] = curr_idf
-
-    def set_weights(self, inverted_file):
-        self.compute_df(inverted_file)
-        n, m = self.vectorspace.shape
-        for document in self.documents:
-            max_tf = document.get_max_tf()
-            for word in document.get_single_words():
-                tf = document.get_tf(word)
-                self.vectorspace[document.get_id(), self.dictionary[word]] = (tf / max_tf) * self.idf[word]
-
-    def compute_query_weight(self, query):
-        n, m = self.vectorspace.shape
-        query_weight = np.zeros((m, 1))
-        max_tf = query.get_max_tf()
-        for word in query.get_single_words():
-            tf = query.get_tf(word)
-            query_weight[self.dictionary[word]] = (tf / max_tf) * self.idf[word]
-
-        return query_weight
-
-    def get_weight(self, i):
-        return self.vectorspace[i, :]
+        if term in self.term_frequency.keys():
+            return self.term_frequency[term]
+        else:
+            return 0.0
 
     def get_terms(self):
-        return self.dictionary.keys()
+        return self.term_frequency.keys()
 
-    def get_document(self, did):
-        return self.documents[did]
+    def get_weight(self, term):
+        if term in self.weights.keys():
+            return self.weights[term]
+        else:
+            return 0.0
 
-    def get_word_id(self, term):
-        return self.dictionary[term]
+    def get_weights(self):
+        return self.weights.values()
 
-def split_word(word, st):
+    def get_top_n_terms(self, n):
+        n = min(n, len(self.weights.keys()))
+        return sorted(self.weights.items(), key = lambda x: x[1], reverse = True)[: n]
+
+    def display_term_index(self, word):
+        print(' D%d:' % self.did, end = '')
+        print('%d' % self.term_index[word][0], end = '')
+        for i in range(1, len(self.term_index[word])):
+            print(',%d' % self.term_index[word][0], end = '')
+        print(' |', end = '')
+
+class InvertedFile(object):
     '''
-        Split the words containing punctuation marks. E.g. Mexico's
-    '''
-    split_notation = ' '
-    position = len(word)
-    for i in range(st, len(word)):
-        letter = word[i]
-        if letter.isdigit() or letter.isalpha():
-            continue
-        split_notation = letter
-        position = i
-        break
-    ret = word[st :].split(split_notation)
-    return ret, position
+        Inverted file index for the documents.
 
-def pre_process(passage):
+        Attrs:
+            index: dictionary, map keywords to a list of document id.
     '''
-        Preprocess the given passage -- one line in the original file
+    def __init__(self, word_file_map):
+        self.index = word_file_map
+
+    def get_documents(self, term):
+        if term in self.index.keys():
+            return self.index[term]
+        else:
+            return None
+
+    def exist(self, term):
+        if term in self.index.keys():
+            return True
+        else:
+            return False
+
+class QueryResult:
     '''
-    candidates = passage.strip()[: -1].split()
-    temp = []
-    words = []
+        Abstract data structure for query result.
 
-    for candidate in candidates:
-        splited_words, pos = split_word(candidate, 0)
-        while pos < len(candidate) - 1:
-            temp.append(splited_words[0])
-            splited_words, pos = split_word(candidate, pos + 1)
-        temp.append(splited_words[0])
+        Attrs:
+            did: int, document id for the matched document.
+            posting_list: list, containing the top five weighted keywords of the
+            matched document, every items containing the keyword and the list of
+            document id. E.g. ['even', [29, 59]].
+            num_keywords: int, number of unique keywords in document.
+            magnitude: float, magnitude of the document vector.
+            sim_score: float, similarity score.
+    '''
+    def __init__(self, did, postinglist, numkeywords, magnitude, simscore):
+        self.did = did
+        self.posting_list = postinglist
+        self.num_keywords = numkeywords
+        self.magnitude = magnitude
+        self.sim_score = simscore
 
-    for word in temp:
-        if len(word) < 4:
-            continue
-        if not word.isalpha():
-            continue
-        if word[-1] == 's':
-            if len(word) < 5:
+    def get_id(self):
+        return self.did
+
+    def get_list(self):
+        return self.posting_list
+
+    def get_num(self):
+        return self.num_keywords
+
+    def get_magnitude(self):
+        return self.magnitude
+
+    def get_sim_score(self):
+        return self.sim_score
+
+class DataManager(object):
+    '''
+        Storage of all the data structure and maintains them.
+
+        Attrs:
+            documents: list, storing all the documents in the system.
+            inverted_file: InvertedFile, the inverted file index for the documents.
+            dictionary: dictionary, map keywords to their term id, which are the
+            indexes in the vector space.
+    '''
+    def __init__(self, word_file_map, documents):
+        self.documents = documents
+        self.inverted_file = InvertedFile(word_file_map)
+        self.dictionary = {}
+        for i, term in enumerate(word_file_map.keys()):
+            self.dictionary[term] = i
+
+        for document in self.documents:
+            did = document.get_id()
+            for word in document.get_terms():
+                idf = math.log(len(documents) / len(word_file_map[word]), 2)
+                max_tf = document.get_max_tf()
+                tf = document.get_tf(word)
+                weight = tf / max_tf * idf
+                self.documents[did].set_weight(word, weight)
+
+    def magnitude(self, vector):
+        accumulate = 0
+        for weight in vector.get_weights():
+            accumulate += weight ** 2
+        return math.sqrt(accumulate)
+
+    def similarity(self, v1, v2):
+        sim = 0.0
+        for word in v2.get_terms():
+            sim += v1.get_weight(word) * v2.get_weight(word)
+
+        sim /= self.magnitude(v1) * self.magnitude(v2)
+        return sim
+
+    def get_query_result(self, query):
+        ret = []
+        rank_list = {}
+        candidates = self.get_documents_by_terms(query.get_terms())
+        for did in candidates:
+            document = self.documents[did]
+            sim = self.similarity(document, query)
+            rank_list[did] = sim
+
+        result = sorted(rank_list.items(), key = lambda x: x[1], reverse = True)[:3]
+
+        for did, sim in result:
+            document = self.documents[did]
+            magnitude = self.magnitude(document)
+            num_terms = len(document.get_terms())
+            words = document.get_top_n_terms(5)
+            postinglist = []
+            for word, weight in words:
+                dids = self.get_documents_by_term(word)
+                postinglist.append([word, dids])
+            ret.append(QueryResult(did, postinglist, num_terms, magnitude, sim))
+
+        return ret
+
+    def get_documents_by_term(self, word):
+        return self.inverted_file.get_documents(word)
+
+    def get_documents_by_terms(self, words):
+        candidates = set()
+        have_illegal_words = False
+        illegal_words = []
+        for word in words:
+            if self.inverted_file.exist(word):
+                candidates.update(self.inverted_file.get_documents(word))
+            else:
+                have_illegal_words = True
+                illegal_words.append(word)
+
+        if have_illegal_words:
+            for word in illegal_words:
+                print('\'%s\' has not been collected in the dictionary.' % word)
+
+        return candidates
+
+    def display_posting_list(self, word, dids):
+        for did in dids:
+            self.documents[did].display_term_index(word)
+
+class VSM(object):
+    '''
+        The controller of the system, interact with upper layers and perform operations.
+
+        Attrs:
+            data_manager: DataManager, storing all the data structure in the system.
+    '''
+    def __init__(self, input_path):
+        word_file_map, documents = self.load_documents(input_path)
+        self.data_manager = DataManager(word_file_map, documents)
+
+    def pre_process(self, passage):
+        passage = passage.strip()
+        for i in range(len(passage)):
+            char = passage[i]
+            if char.isalpha() or char.isdigit():
                 continue
-            else:
-                word = word[: -1]
-        words.append(word)
+            if char == ' ':
+                continue
+            passage = passage[: i] + ' ' + passage[i + 1 :]
 
-    return words
+        candidates = passage.split()
 
-def load_documents(input_path):
-    '''
-        Load documents into the memory by given path
-    '''
-    word_file_map = {}
-    word_indexes = []
-    documents = []
+        words = []
+        for candidate in candidates:
+            if len(candidate) < 4:
+                continue
+            if not candidate.isalpha():
+                continue
+            if candidate[-1] == 's':
+                if len(candidate) < 5:
+                    continue
+                else:
+                    candidate = candidate[: -1]
+            words.append(candidate)
 
-    input_collection = open(input_path, 'r')
-    for line in input_collection:
-        if len(line) < 2:
-            continue
-        words = pre_process(line)
-        curr_did = len(documents)
-        document = Vector(words, curr_did)
-        documents.append(document)
+        return words
 
-        local_word_index = {}
-        for i in range(len(words)):
-            word = words[i]
-            if word not in local_word_index.keys():
-                local_word_index[word] = [i]
-            else:
-                local_word_index[word].append(i)
+    def load_documents(self, input_path):
+        word_file_map = {}
+        documents = []
 
-            if word not in word_file_map.keys():
-                word_file_map[word] = [curr_did]
-            elif word_file_map[word][-1] != curr_did:
-                word_file_map[word].append(curr_did)
+        input_collection = open(input_path, 'r')
+        for line in input_collection:
+            if len(line) < 2:
+                continue
 
-        word_indexes.append(local_word_index)
+            words = self.pre_process(line)
+            curr_id = len(documents)
+            document = Vector(words, curr_id)
+            documents.append(document)
 
-    return word_file_map, word_indexes, documents
+            for word in words:
+                if word not in word_file_map.keys():
+                    word_file_map[word] = [curr_id]
+                elif word_file_map[word][-1] != curr_id:
+                    word_file_map[word].append(curr_id)
 
-def initialize(input_path):
-    '''
-        Initialize the data structures
-    '''
-    word_file_map, word_indexes, documents = load_documents(input_path)
-    inverted_file = InvertedFile(word_file_map, word_indexes)
-    vspace = VectorSpace(documents, word_file_map.keys())
-    vspace.set_weights(inverted_file)
+        return word_file_map, documents
 
-    return inverted_file, vspace
+    def display_result(self, result):
+        print('DID: %d' % result.get_id())
+        for word, dids in result.get_list():
+            print('%-8s -> |' % word, end = '')
+            self.data_manager.display_posting_list(word, dids)
+            print()
+        print('Number of unique keywords in document: %s'
+              % result.get_num())
+        print('Magnitude of the document vector: %.2f'
+              % result.get_magnitude())
+        print('Similarity score: %.2f' % result.get_sim_score())
 
-def L2_norm(vec):
-    return np.linalg.norm(vec, ord=2)
+    def do_query(self, query):
+        print('Query: %s' % query)
+        print('----------------------------------------')
+        start = time.time()
 
-# def my_norm(vec):
-#     result = 0
-#     for elem in vec:
-#         result += elem ** 2
-#     return math.sqrt(result)
+        query = Vector(query)
+        for word in query.get_terms():
+            query.set_weight(word, query.get_tf(word))
 
-def do_query(query, query_weight, inverted_file, vspace):
-    documents = set()
-    similarities = {}
-    for word in query.get_single_words():
-        candidate = inverted_file.get_documents(word)
-        documents.update(candidate)
+        query_result = self.data_manager.get_query_result(query)
+        for result in query_result:
+            self.display_result(result)
+            print('----------------------------------------')
 
-    length_query = L2_norm(query_weight)
-    for did in documents:
-        weights = vspace.get_weight(did)
-        length_document = L2_norm(weights)
-        similarity = np.dot(weights, query_weight)[0]
-        similarity /= length_query * length_document
-        similarities[did] = similarity
+        end = time.time()
+        print('Spended Time: %.6fs\n' % (end - start))
 
-    ret = sorted(similarities.items(), key = lambda x: x[1], reverse = True)[: 3]
-    return ret
+    def batch_query(self, input_path):
+        input_queries = open(input_path, 'r')
+        for line in input_queries:
+            query = self.pre_process(line)
 
-def get_top_n(did, vspace):
-    weights = vspace.get_weight(did)
-    temp = {}
-    relevant_words = vspace.get_document(did).get_single_words()
-    for word in relevant_words:
-        word_id = vspace.get_word_id(word)
-        word_weight = weights[word_id]
-        temp[word] = word_weight
-
-    ret = sorted(temp.items(), key = lambda x: x[1], reverse = True)[: 5]
-    return ret
-
-def display_result(result, inverted_file, vspace):
-    for did, similarity in result:
-        print('DID: %s' % did)
-        top_n = get_top_n(did, vspace)
-        for word, weight in top_n:
-            inverted_file.display(word)
-        print('Number of unique keywords in document: %s' % len(vspace.get_document(did).get_single_words()))
-        print('Magnitude of the document vector: %s' % L2_norm(vspace.get_weight(did)))
-        print('Similarity score: %s\n' % similarity)
+            self.do_query(query)
 
 def main():
-    INPUT_PATH = './collection-100.txt'
-    QUERY_PATH = './query-10.txt'
+    collection = 'collection-100.txt'
+    queries = 'query-10.txt'
 
-    inverted_file, vspace = initialize(INPUT_PATH)
-    weights = vspace.get_weight(0)
-    # print(L2_norm(weights), my_norm(weights))
-    # words = vspace.get_terms()
-    # for word in words:
-    #     print('%15s | %s' % (word, weights[vspace.get_word_id(word)]))
-
-    query = Vector(['bank'])
-    query_weight = vspace.compute_query_weight(query)
-    # print(query_weight)
-    result = do_query(query, query_weight, inverted_file, vspace)
-    display_result(result, inverted_file, vspace)
+    vsm_object = VSM(INPUT_PATH + collection)
+    # vsm_object.do_query(['is', 'a', 'bank'])
+    # vsm_object.batch_query(INPUT_PATH + queries)
 
 if __name__ == '__main__':
     main()
